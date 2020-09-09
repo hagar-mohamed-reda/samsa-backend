eac7a91b819b6e9cd37ce5530d88e446e61561f0
Mustafa Elgammal <gogo202046@yahoo.com>
2020-09-09 12:42:31 +0200

    second push

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/api_auth', function(Illuminate\Http\Request $request) { 
     return responseJson(0, __('login first'));
 });
+
+
+Route::group(['middleware' => 'api_auth'], function() {
+    Route::get('/', 'DashboardController@index')->name('dashboard');
+    //start nationalities
+    Route::get('nationalities', 'nationalityController@index')->name('nationalities');
+
+    //end nationalities
+    Route::resource('roles', 'RoleController');
+
+    Route::resource('cities', 'CityController');
+    Route::resource('countries', 'CountryController');
+    Route::resource('governments', 'GovernmentController');
+    Route::get('government/{country_id}', 'GovernmentController@getGovernments');
+    Route::resource('languages', 'LanguageController');
+    Route::resource('profile', 'UserProfileController');
+    Route::put('update-password/{user_id}', 'UserProfileController@updatePassword')->name('update-password');
+    Route::resource('relations', 'RelativeRelationController');
+
+
+    Route::post('setting/update/{setting}', 'SettingController@update')->name('SettingUpdate');
+
+});
