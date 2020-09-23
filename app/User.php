e347bb3e0c3897641011a6b2c1287b338aab4a51
Mustafa Elgammal <gogo202046@yahoo.com>
2020-09-23 09:35:48 +0200

    user -> role

--- app/User.php
+++ app/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'username', 'image', 'phone', 'api_token'
+        'name', 'email', 'password', 'username', 'image', 'phone'
     ];
 
     
